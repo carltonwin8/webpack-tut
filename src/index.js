f2c72427531797f8c8167c93f3293a065e584d06
Carlton Joseph <carlton.joseph@gmail.com>
2017-01-05 05:46:57 -0800

    add custom dev server with dev and production builds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const messages = require('./messages.js');
 const app = document.getElementById('app');
 app.innerHTML = '<p>' + messages.hi + ' ' + messages.event + '</p>';
+
+if(module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const entry = PRODUCTION
+  ?
+    [
+      './src/index.js'
+    ]
+  :
+    [
+      './src/index.js',
+      'webpack/hot/dev-server',
+      'webpack-dev-server/client?http://localhost:8080'
+    ]
+  ;
+
+const plugins = PRODUCTION
+  ?
+    [
+    ]
+  :
+    [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  ;
+
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: entry,
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: "bundle.js"
-  }
+  },
+  plugins: plugins
 }
