d813988e184efc621ba7870c4d25b627ae7f053b
Carlton Joseph <carlton.joseph@gmail.com>
2017-01-08 23:31:21 -0800

    add es6 module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const messages = require('./messages.js');
+import Button from './button';
 
-const newMessage = () => (`<p>${messages.hi} ${messages.event}</p>`);
+//const newMessage = () => (`<p>${messages.hi} ${messages.event} 1</p>`);
+const newMessage = () => (Button.button);
 
 const app = document.getElementById('app');
 app.innerHTML = newMessage();
+Button.attachEl();
 
 if(module.hot) {
   module.hot.accept();
